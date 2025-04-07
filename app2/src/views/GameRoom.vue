07bc36cafbff4e373bc2b66e1b3b3a260ee7c865
ryans439 <ryans439@nycstudents.net>
2025-04-07 14:03:02 -0400

    Update GameRoom.vue

--- app2/src/views/GameRoom.vue
+++ app2/src/views/GameRoom.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <button class= "tailwindstuff" v-for="game in games" link = "BlackJack.vue">Black Jack</button>
+        <button class= "tailwindstuff"  link = "BlackJack.vue">Black Jack</button>
+        <button class= "tailwindstuff"  link = "SlotMachine.vue">Slot Machine</button> 
     </div>
 </template>
 
 <script setup>
+
 //room with all the games to click on
 </script>
 
