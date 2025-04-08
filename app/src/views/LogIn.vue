7274e8bf0cfa7cdd6ef1891858d2fb8ce2eb8909
Laithanator <144480666+Laithanator@users.noreply.github.com>
2025-04-08 14:02:28 -0400

    ehhhejhrt

--- app/src/views/LogIn.vue
+++ app/src/views/LogIn.vue
@@ -1,21 +1,11 @@
 <template>
-<<<<<<< HEAD
   <div>
-<TestHeader />
+
   </div>
 </template>
 
 <script setup>
-import TestHeader from '../components/TestHeader.vue'
-=======
-    <div>
 
-    </div>
-</template>
-
-<script setup>
-
->>>>>>> main
 </script>
 
 <style scoped>
