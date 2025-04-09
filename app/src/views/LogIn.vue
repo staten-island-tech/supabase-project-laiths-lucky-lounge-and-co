43659b180c53d2851dc75fae3d483eca95738842
Laithanator <144480666+Laithanator@users.noreply.github.com>
2025-04-09 14:02:44 -0400

    g5eahr5s

--- app/src/views/LogIn.vue
+++ app/src/views/LogIn.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-
+    <TestHeader />
+    <RouterLink to="/homepage">Game Room</RouterLink>
   </div>
 </template>
 
 <script setup>
-
+import TestHeader from '@/components/TestHeader.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
