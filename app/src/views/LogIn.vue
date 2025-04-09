113031e0456e9080d2b6b7bcf8071742b43fcd80
ryans439 <ryans439@nycstudents.net>
2025-04-09 13:53:31 -0400

    Update LogIn.vue

--- app/src/views/LogIn.vue
+++ app/src/views/LogIn.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-
+    <div>
+      <h1>Log into Laith's Lucky Lounge</h1>
+      <input type="text" id="Username" placeholder="Username" />
+      <input type="password" id="Password" placeholder="Password" />
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
