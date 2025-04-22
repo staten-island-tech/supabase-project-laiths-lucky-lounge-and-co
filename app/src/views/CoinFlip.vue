af7f442104b6bdb30bd88f78dc25ac95ec153903
ryans439 <ryans439@nycstudents.net>
2025-04-22 13:42:49 -0400

    Update CoinFlip.vue

--- app/src/views/CoinFlip.vue
+++ app/src/views/CoinFlip.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-
-    </div>
+  <div></div>
 </template>
 
 <script setup>
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
+let randomNumber = getRandomIntInclusive(1, 10)
+if (randomNumber > 5) {
+  console.log('Heads')
+} else {
+  console.log('Tails')
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
