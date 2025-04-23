209563d4415cf972d2ea654ba4d149c0fd096a9f
ryans439 <ryans439@nycstudents.net>
2025-04-23 08:46:40 -0400

    Update CoinFlip.vue

--- app/src/views/CoinFlip.vue
+++ app/src/views/CoinFlip.vue
@@ -3,24 +3,21 @@
     <h1>Coin Flip</h1>
     <input v-model="inputNumber" placeholder="Your betting amount!" type="number" />
     <p>You're betting {{ inputNumber }}</p>
-    <button @click="randomNumber = getRandomIntInclusive(1, 10)">Flip Coin!</button>
+    <button @click="flipcoin()">Flip Coin!</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 const inputNumber = ref(0)
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
 
-let randomNumber = getRandomIntInclusive(1, 10)
-if (randomNumber > 5) {
-  console.log('Heads')
-} else {
-  console.log('Tails')
+function flipcoin() {
+  let randomNumber = Math.random()
+  if (randomNumber > 0.5) {
+    console.log('Heads')
+  } else {
+    console.log('Tails')
+  }
 }
 </script>
 
