633e9bed1206ffee9fbdc1502b27f7f7a2a55689
Laithanator <144480666+Laithanator@users.noreply.github.com>
2025-04-29 14:01:18 -0400

    g5w465j

--- app/src/views/LogIn.vue
+++ app/src/views/LogIn.vue
@@ -4,11 +4,7 @@
       <h1>Log into Laith's Lucky Lounge</h1>
       <input type="text" id="Username" placeholder="Username" />
       <input type="password" id="Password" placeholder="Password" />
-      <div class="box">
-        <p>Ni Hao</p>
-      </div>
     </div>
-    <TestHeader />
     <RouterLink to="/homepage">Game Room</RouterLink>
   </div>
 </template>
