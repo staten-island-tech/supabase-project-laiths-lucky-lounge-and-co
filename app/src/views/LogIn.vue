f6f0c2cc0c8c6d9e8602f144b8e0aa5e0c9618be
Laithanator <144480666+Laithanator@users.noreply.github.com>
2025-06-08 02:23:49 -0400

    yjftjfgj

--- app/src/views/LogIn.vue
+++ app/src/views/LogIn.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="">
     <SignIn />
-    <RouterLink
-      to="/signup"
-      class="mt-4 bg-yellow-500 hover:bg-yellow-600 text-center text-white font-semibold py-2 rounded-md transition"
-    >
-      Sign Up
-    </RouterLink>
   </div>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router'
 import SignIn from '../components/SignIn.vue'
 </script>
 
